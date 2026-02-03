1fc28aa6de48dd6dbd7b5fc92317bb002c2144fa
Pradeep Yadav <35965737+ydv-prdp@users.noreply.github.com>
2026-02-03 21:08:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ app.post("/webhook",(req,res)=>{ //i want some
 
                axios({
                    method:"POST",
-                   url:"https://graph.facebook.com/v13.0/"+phon_no_id+"/messages?access_token="+token,
+                   url:"https://graph.facebook.com/v22.0/"+phon_no_id+"/messages?access_token="+token,
                    data:{
                        messaging_product:"whatsapp",
                        to:from,
